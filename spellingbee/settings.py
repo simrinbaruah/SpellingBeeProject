da6be6c2634f7052fc170c9ae141ad5eda76986f
simrinbaruah <simrinbaruah@gmail.com>
2022-02-13 19:09:49 +0530

    Update settings.py

--- spellingbee/settings.py
+++ spellingbee/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'spellingbee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
